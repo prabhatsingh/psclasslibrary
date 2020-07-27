066ad9ac75296f2ff40a9d29e194b62384f68e59
Prabhat Singh <prabhat.singh@canadalife.com>
2020-07-27 13:43:58 -0500

    Add project files.

---
+++ PsUtilities/ImageUtilities.cs
@@ -0,0 +1,90 @@
+﻿using ImageProcessor;
+using ImageProcessor.Imaging.Formats;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace PsUtilities
+{
+    public class ImageUtilities
+    {
+        public Image GetOptimizedImage(string inputfile, int finalsize = 1024, float hdpi = 0, float vdpi = 0)
+        {
+            ISupportedImageFormat format = new JpegFormat();
+            Size size = new Size(finalsize, 0);
+
+            using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true))
+            {
+
+                using (Image sourceImg = imageFactory.Load(inputfile).Resize(size).Format(format).Image)
+                {
+                    var clonedImg = new Bitmap(sourceImg.Width, sourceImg.Height, PixelFormat.Format32bppArgb);
+
+                    clonedImg.SetResolution(hdpi == 0 ? Image.FromFile(inputfile).HorizontalResolution : hdpi, vdpi == 0 ? Image.FromFile(inputfile).VerticalResolution : vdpi);
+
+                    using (var copy = Graphics.FromImage(clonedImg))
+                    {
+                        copy.DrawImage(sourceImg, 0, 0);
+                    }
+
+                    return clonedImg;
+                }
+            }
+        }
+
+        public string OptimizeHere(string inputfile, int finalsize = 1024, float hdpi = 0, float vdpi = 0, string outputpath = "")
+        {
+            if (string.IsNullOrEmpty(outputpath))
+                outputpath = Path.GetDirectoryName(inputfile) + Path.GetFileNameWithoutExtension(inputfile) + "_Optimized" + Path.GetExtension(inputfile);
+
+            GetOptimizedImage(inputfile, finalsize, hdpi, vdpi).Save(outputpath);
+            return outputpath;
+        }
+
+        public string OptimizeToTemp(string inputfile, int finalsize = 1024, float hdpi = 0, float vdpi = 0, string outputpath = "")
+        {
+            if (string.IsNullOrEmpty(outputpath))
+                outputpath = Path.GetTempPath() + Path.GetFileNameWithoutExtension(inputfile) + "_Optimized" + Path.GetExtension(inputfile);
+
+            GetOptimizedImage(inputfile, finalsize, hdpi, vdpi).Save(outputpath);
+            return outputpath;
+        }
+
+        public Image Rotate(string inputfile, float rotation)
+        {
+            using (ImageFactory imageFactory = new ImageFactory(preserveExifData: true).Resolution((int)Image.FromFile(inputfile).HorizontalResolution, (int)Image.FromFile(inputfile).VerticalResolution))
+            {
+                using (Image sourceImg = imageFactory.Load(inputfile).RotateBounded(rotation, true).Image)
+                {
+                    var clonedImg = new Bitmap(sourceImg.Width, sourceImg.Height, PixelFormat.Format32bppArgb);
+                    clonedImg.SetResolution(Image.FromFile(inputfile).HorizontalResolution, Image.FromFile(inputfile).VerticalResolution);
+
+                    using (var copy = Graphics.FromImage(clonedImg))
+                    {
+                        copy.DrawImage(sourceImg, 0, 0);
+                    }
+
+                    return clonedImg;
+                }
+            }
+        }
+
+        public string RotateHere(string inputfile, float rotation, string outputpath = "")
+        {
+            if (string.IsNullOrEmpty(outputpath))
+                outputpath = Path.GetDirectoryName(inputfile) + Path.GetFileNameWithoutExtension(inputfile) + "_Rotated" + Path.GetExtension(inputfile);
+
+            Rotate(inputfile, rotation).Save(outputpath);
+            return outputpath;
+        }
+
+        public string RotateToTemp(string inputfile, float rotation, string outputpath = "")
+        {
+            if (string.IsNullOrEmpty(outputpath))
+                outputpath = Path.GetTempPath() + Path.GetFileNameWithoutExtension(inputfile) + "_Rotated" + Path.GetExtension(inputfile);
+
+            Rotate(inputfile, rotation).Save(outputpath);
+            return outputpath;
+        }
+    }
+}
---
+++ PsUtilities/PdfUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PsUtilities
+{
+    public class PdfUtilities
+    {
+        public static void PrintPdfToXps(string inputfilename)
+        {
+            string acropath = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\AcroRd32.exe").GetValue("").ToString();
+            var printer = System.Drawing.Printing.PrinterSettings.InstalledPrinters.Cast<string>().ToList().Find(f => f.Contains("XPS"));
+            System.Diagnostics.Process.Start(acropath, string.Format("/p /h /t \"{0}\" \"{1}\"", inputfilename, printer));
+        }
+    }
+}
